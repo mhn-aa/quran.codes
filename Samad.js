08dd4d2184bab452435096451f685925be4f47df
mhn-aa <mahanmirjamali@gmail.com>
2022-03-24 19:22:46 +1100

    Samad

---
+++ Samad.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Samad() {
+  return (
+    <div>
+      <h1>Ikhlas</h1>
+      <h2>بسم الله الرحمن الرحیم</h2>
+      <a href="">
+        <p>قل هو الله احد</p>
+        <p>الله صمد</p>
+        <p>لم یلد ولم یولد</p>
+        <p>ولم یکن له کفوا احد</p>
+      </a>
+    </div>
+  );
+}
