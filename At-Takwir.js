7ed03a43fb46c2c75f042ff44a8790a42a3d068a
mhn-aa <mahanmirjamali@gmail.com>
2022-03-28 18:13:31 +1100

    55 - Ghamar

---
+++ At-Takwir.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function AtTakwir() {
+  return (
+    <div>
+      <div className="AtTakwir">
+        <h1>AtTakwir</h1>
+      </div>
+
+      <div className="p1">
+        <p>إِذَا ٱلشَّمْسُ كُوِّرَتْ</p>
+      </div>
+    </div>
+  );
+}
+
+export default AtTakwir;
