92e2fa804f43ff6b9b74f1794ea55d26b2d2d195
mhn-aa <mahanmirjamali@gmail.com>
2022-04-01 19:38:30 +1100

    S-30 A-10

---
+++ Al-Rum.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function AlRum() {
+  return (
+    <div>
+      <h1>Al-Rum</h1>
+      <h2>30-10</h2>
+      <p>
+        ثُمَّ كَانَ عَـٰقِبَةَ ٱلَّذِينَ أَسَـٰٓـُٔوا۟ ٱلسُّوٓأَىٰٓ أَن
+        كَذَّبُوا۟ بِـَٔايَـٰتِ ٱللَّهِ وَكَانُوا۟ بِهَا يَسْتَهْزِءُونَ
+      </p>
+      <p>
+        Then most evil was the end of the evildoers for denying and mocking the
+        signs of Allah.
+      </p>
+    </div>
+  );
+}
+
+export default AlRum;
