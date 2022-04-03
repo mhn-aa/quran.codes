aa50daff5f433771e317a5812acd37f2ab7248b6
mhn-aa <mahanmirjamali@gmail.com>
2022-04-03 16:43:06 +1000

    Al-ahzab 33-56

---
+++ Al-ahzab.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Alahzab() {
+  <div>
+    <p>
+      إِنَّ ٱللَّهَ وَمَلَـٰٓئِكَتَهُۥ يُصَلُّونَ عَلَى ٱلنَّبِىِّ ۚ
+      يَـٰٓأَيُّهَا ٱلَّذِينَ ءَامَنُوا۟ صَلُّوا۟ عَلَيْهِ وَسَلِّمُوا۟
+      تَسْلِيمًا
+    </p>
+    <p>
+      Indeed, Allah showers His blessings upon the Prophet, and His angels pray
+      for him. O believers! Invoke Allah’s blessings upon him, and salute him
+      with worthy greetings of peace.
+    </p>
+  </div>;
+}
+
+export default Alahzab;
