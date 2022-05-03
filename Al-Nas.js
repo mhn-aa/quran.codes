7c506d7c9da2f3a8b601544c35641d4b5375963e
mhn-aa <mahanmirjamali@gmail.com>
2022-05-03 20:26:53 +1000

    AlNas

---
+++ Al-Nas.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function AlNas() {
+  return (
+    <div>
+      <p>قل اعوذ برب الناس</p>
+      <p>ملک الناس</p>
+      <p>اله الناس</p>
+      <p>من شر الوسواس الخناس</p>
+      <p>الذی یوسوس فی صدور الناس</p>
+      <p>من الجنه و الناس</p>
+    </div>
+  );
+}
+
+export default AlNas;
