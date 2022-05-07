3fe4b19e0f5d1f07feb8b7aad3e0dacf8fcbd6ff
mhn-aa <mahanmirjamali@gmail.com>
2022-05-07 13:53:20 +1000

    Adh-Dhariyat

---
+++ Adh-Dhariyat.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AdhDhariyat() {
+  return (
+    <div>
+      <a href="https://quran.com/51">Adh-Dhariyat</a>
+    </div>
+  );
+}
+
+export default AdhDhariyat;
